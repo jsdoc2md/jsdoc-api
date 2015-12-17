65561daa295ac75a8abeec5bb38e8e9e6814fb33
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-12-17 13:37:53 +0000

    first explain(source) test passing

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
 'use strict'
+const commandLineArgs = require('command-line-args')
+const jsdoc = require('../')
+
+const cli = commandLineArgs([
+  { name: 'src', type: String, multiple: true },
+  { name: 'explain', type: Boolean }
+])
+
+const options = cli.parse()
+
+console.log(jsdoc(options))
--- test/test.js
+++ test/test.js
@@ -1,9 +1,28 @@
 var test = require('tape')
 var jsdoc = require('../')
+var path = require('path')
+var fs = require('fs')
 
-test('async', function(t){
-  t.plan(1)
-  jsdoc({ src: __dirname + '/fixture/class.js', explain: true }).then(data => {
-    t.ok(data.length > 1)
-  })
+function getFixture (filepath) {
+  return fs.readFileSync(
+    path.resolve(__dirname, '..', 'node_modules', 'jsdoc2md-testbed', 'src', filepath),
+    'utf-8'
+  )
+}
+function getOutput (filepath) {
+  return fs.readFileSync(
+    path.resolve(__dirname, '..', 'node_modules', 'jsdoc2md-testbed', 'output', '1. jsdoc-api', filepath),
+    'utf-8'
+  )
+}
+
+test('explain', function(t){
+  var result = jsdoc.explain(getFixture('global/class-all.js'))
+  var fixtureOutput = JSON.parse(getOutput('global/class-all.json'))
+  result.forEach(i => { delete i.meta; delete i.files })
+  fixtureOutput.forEach(i => { delete i.meta; delete i.files })
+
+  t.ok(typeof result === 'object')
+  t.deepEqual(result, fixtureOutput)
+  t.end()
 })
