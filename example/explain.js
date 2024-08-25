14009404816e79f6992fdf383525980b53a73f10
Lloyd Brookes <lloyd.brookes@gmail.com>
2024-08-25 16:40:38 +0100

    Avoid passing '--pedantic false' into jsdoc. Fixes #22.

--- example/explain.js
+++ example/explain.js
@@ -4,5 +4,5 @@ util.inspect.defaultOptions.depth = 6
 util.inspect.defaultOptions.breakLength = process.stdout.columns
 util.inspect.defaultOptions.maxArrayLength = Infinity
 
-const data = await jsdoc.explain({ files: process.argv.slice(2), cache: true })
+const data = await jsdoc.explain({ files: process.argv.slice(2), cache: true, pedantic: true })
 console.log(data)
