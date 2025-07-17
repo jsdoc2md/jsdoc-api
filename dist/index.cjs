dfe32228e7fa8d08b0c3784ccc53737d587e59b5
Lloyd Brookes <opensource@75lb.com>
2025-07-17 16:36:05 +0100

    Fix regression where files passed directly to jsdocApi.render() were sorted unnecessarily #33

--- dist/index.cjs
+++ dist/index.cjs
@@ -6,7 +6,8 @@ var path = require('path');
 var fs = require('node:fs');
 var os = require('os');
 var crypto = require('crypto');
-var FileSet = require('file-set');
+var fg = require('fast-glob');
+var fs$1 = require('fs');
 var assert = require('assert');
 var walkBack = require('walk-back');
 var currentModulePaths = require('current-module-paths');
@@ -36,6 +37,82 @@ class TempFile {
     /* No longer using os.tmpdir(). See: https://github.com/jsdoc2md/jsdoc-api/issues/19 */
     fs.mkdirSync(TempFile.tempFileDir, { recursive: true });
     fs.mkdirSync(TempFile.cacheDir, { recursive: true });
+  }
+}
+
+class FileSet {
+  constructor () {
+    /* validation */
+    if (arguments.length) {
+      throw new Error('new Fileset() does not require any arguments')
+    }
+
+    /** • fileSet.files :string[]
+    ≈ The existing files found.
+    */
+    this.files = [];
+
+    /** • fileSet.dirs :string[]
+    ≈ The existing directories found. Directory paths will always end with `'/'`.
+    */
+    this.dirs = [];
+
+    /** • fileSet.notExisting :string[]
+    ≈ Paths which were not found.
+    */
+    this.notExisting = [];
+  }
+
+  /** ø fileSet.add(patterns)
+  ≈ Add file patterns to the set.
+  • [patterns] :string|string[] - One or more file paths or glob expressions to inspect.
+  */
+  async add (files, options = {}) {
+    files = arrayify(files);
+    for (let file of files) {
+      /* Force all incoming file paths and glob expressions to use posix separators */
+      file = os.platform() === 'win32'
+        ? file.replace(/\\/g, path.posix.sep)
+        : file;
+      try {
+        const stat = await fs$1.promises.stat(file);
+        if (stat.isFile() && !this.files.includes(file)) {
+          this.files.push(file);
+        } else if (stat.isDirectory() && !this.dirs.includes(file)) {
+          this.dirs.push(file.endsWith(path.posix.sep) ? file : `${file}${path.posix.sep}`);
+        }
+      } catch (err) {
+        if (err.code === 'ENOENT') {
+          if (fg.isDynamicPattern(file)) {
+            const found = await fg.glob(file, { onlyFiles: false, markDirectories: true });
+            if (found.length) {
+              if (options.globResultSortFn) {
+                found.sort(options.globResultSortFn);
+              }
+              for (const match of found) {
+                if (match.endsWith(path.posix.sep)) {
+                  if (!this.dirs.includes(match)) this.dirs.push(match);
+                } else {
+                  if (!this.files.includes(match)) this.files.push(match);
+                }
+              }
+            } else {
+              if (!this.notExisting.includes(file)) this.notExisting.push(file);
+            }
+          } else {
+            if (!this.notExisting.includes(file)) this.notExisting.push(file);
+          }
+        } else {
+          throw err
+        }
+      }
+    }
+  }
+
+  clear () {
+    this.files = [];
+    this.dirs = [];
+    this.notExisting = [];
   }
 }
 
@@ -74,10 +151,9 @@ class JsdocCommand {
 
   async execute () {
     this.inputFileSet = new FileSet();
-    await this.inputFileSet.add(this.options.files);
     /* node-glob v9+ (used by file-set) no longer sorts the output by default. We will continue to sort the file list, for now, as it's what the user expected by default. The user's system locale is used. */
     const collator = new Intl.Collator();
-    this.inputFileSet.files.sort(collator.compare);
+    await this.inputFileSet.add(this.options.files, { globResultSortFn: collator.compare });
 
     if (this.options.source.length) {
       this.tempFiles = this.options.source.map(source => new TempFile(source));
